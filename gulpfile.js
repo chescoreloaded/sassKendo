6ee7d64c298a67c10f5e26ffb4a9852c5033d47d
Chesco <ingfranciscoroldan@gmail.com>
2017-01-10 20:47:56 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp        = require('gulp'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("app/scss/*.scss")
+        .pipe(sass())
+        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest("app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
